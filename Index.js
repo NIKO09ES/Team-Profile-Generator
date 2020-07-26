d3bb570753aab8bbabe3ebb181e7d38c5b9fda39
NIKO09ES <nicolasesquivelruiz@gmail.com>
2020-07-25 23:00:05 -0500

    add index and generate-site functions

---
+++ Index.js
@@ -0,0 +1,263 @@
+const { writeFile, copyFile } = require('./utils/generate-site.js');
+const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+//  arrays for the manager, engineer and intern
+managerData = [];
+engineerData = [];
+internData = [];
+
+
+const promptManager = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the team manager name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the team manager name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the team manager ID number? (Required)',
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the team manager ID number!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the team manager Email? (Required)',
+            validate: idEmail => {
+                if (idEmail) {
+                    return true;
+                } else {
+                    console.log('Please enter the team manager Email!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is the team manager office number? (Required)',
+            validate: officeNumberInput => {
+                if (officeNumberInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the team manager office number!');
+                    return false;
+                }
+            }
+        },
+    ])
+    .then(input => {
+        const {name, id, email, officeNumber} = input;
+
+        const manager = new Manager(name, id, email, officeNumber);
+        console.table(manager);
+        managerData.push(manager);
+        promptChoice();
+    })
+};
+
+const promptChoice = () => {
+    console.log(`
+    ==================
+    Add a New Employee
+    ==================
+    `);
+
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'Choice',
+            message: 'please select one of the following options',
+            choices: ['Engineer', 'Intern', 'Done']
+        },
+    ])
+        .then(input => {
+            if (input.Choice === 'Engineer') {
+                promptEngineer();
+            } else if (input.Choice === 'Intern') {
+                promptIntern();
+            } else {
+                console.log('ok')
+                // writeFile();
+            }
+        });
+}
+
+const promptEngineer = () => {
+    console.log(`
+    ==================
+    Add a New Engineer
+    ==================
+    `);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the engineer name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the engineer name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the engineer ID number? (Required)',
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the engineer ID number!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the engineer Email? (Required)',
+            validate: idEmail => {
+                if (idEmail) {
+                    return true;
+                } else {
+                    console.log('Please enter the engineer Email!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is the engineer GitHub username? (Required)',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the engineer GitHub username!');
+                    return false;
+                }
+            }
+        },
+    ])
+    .then(input => {
+        const {name, id, email, github} = input;
+
+        const engineer = new Engineer(name, id, email, github);
+        console.table(engineer);
+        engineerData.push(engineer);
+        promptChoice();
+    })
+};
+
+const promptIntern = () => {
+    console.log(`
+    ==================
+    Add a New Intern
+    ==================
+    `);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the intern name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the intern name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the intern ID number? (Required)',
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the intern ID number!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the intern Email? (Required)',
+            validate: idEmail => {
+                if (idEmail) {
+                    return true;
+                } else {
+                    console.log('Please enter the intern Email!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What is the intern school? (Required)',
+            validate: schoolInput => {
+                if (schoolInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the intern school!');
+                    return false;
+                }
+            }
+        },
+    ])
+    .then(input => {
+        const {name, id, email, school} = input;
+
+        const intern = new Intern(name, id, email, school);
+        console.table(intern);
+        internData.push(intern);
+        promptChoice();
+    })
+};
+
+promptManager()
+
+module.exports = {managerData, engineerData, internData}
+//   .then(promptProject)
+//   .then(portfolioData => {
+//     return generatePage(portfolioData);
+//   })
+//   .then(pageHTML => {
+//     return writeFile(pageHTML);
+//   })
+//   .then(writeFileResponse => {
+//     console.log(writeFileResponse);
+//     return copyFile();
+//   })
+//   .then(copyFileResponse => {
+//     console.log(copyFileResponse);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
---
+++ src/page-template.js
@@ -0,0 +1,105 @@
+const { engineerData } = require("../Index");
+
+module.exports = templateData => {
+    // Create the project section
+    const generateEngineer = engineerData => {
+        return `
+        <section class="my-3" id="portfolio">
+          <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+          <div class="flex-row justify-space-between">
+          ${engineerData
+                .filter(({ feature }) => feature)
+                .map(({ name, id, email, github }) => {
+                    return `
+              <div class="card" style="width: 18rem;">
+                <div class="card-header">
+                    ${name}
+                    <span>Engineer</span>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${id}</li>
+                    <li class="list-group-item">Email: ${email}</li>
+                    <li class="list-group-item">GirHub: ${github}</li>
+                </ul>
+              </div>
+            `;
+                })
+                .join('')}
+    
+            ${engineerData
+                .filter(({ feature }) => feature)
+                .map(({ name, id, email, github }) => {
+                    return `
+                  <div class="card" style="width: 18rem;">
+                    <div class="card-header">
+                        ${name}
+                        <span>Engineer</span>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">ID: ${id}</li>
+                        <li class="list-group-item">Email: ${email}</li>
+                        <li class="list-group-item">GirHub: ${github}</li>
+                    </ul>
+                  </div>
+            `;
+                })
+                .join('')}
+          </div>
+        </section>
+      `;
+    };
+
+    // create the about section
+    const generateAbout = aboutText => {
+        if (!aboutText) {
+            return '';
+        }
+
+        return `
+      <section class="my-3" id="about">
+        <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+        <p>${aboutText}</p>
+      </section>
+    `;
+    };
+
+
+    // destructure page data by section
+    const { projects, about, ...header } = templateData;
+
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+  
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+      <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="style.css">
+    </head>
+  
+    <body>
+      <header>
+        <div class="container flex-row justify-space-between align-center py-3">
+          <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
+          <nav class="flex-row">
+            <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${
+        header.github
+        }">GitHub</a>
+          </nav>
+        </div>
+      </header>
+      <main class="container my-5">
+        ${generateAbout(about)}
+        ${generateProjects(projects)}
+      </main>
+      <footer class="container text-center py-3">
+        <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
+      </footer>
+    </body>
+    </html>
+    `;
+};
