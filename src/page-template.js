3605e2084565eca5ae5b1f41ecde9dbe24187ad2
NIKO09ES <nicolasesquivelruiz@gmail.com>
2020-07-26 15:41:10 -0500

    add style

--- src/page-template.js
+++ src/page-template.js
@@ -1,14 +1,13 @@
 
 
 module.exports.generators = {
-    manager: ({ name, id, email, officeNumber }) => {
-        return `
-        <section class="my-3" id="manager">
-          <div class="flex-row justify-space-between">
-          <div class="card border border-info" style="width: 18rem;">
-                <div class="card-header">
-                    ${name}
-                    <i class="icon large material-icons">pets</i>
+  manager: ({ name, id, email, officeNumber }) => {
+    return `
+          <div class="card col-3 border border-info" style="width: 18rem;">
+                <div class="card-header p-3 mb-2 bg-primary text-white text-center">
+                    <h2>${name}</h2>
+                    </br>
+                    <i class="icon large material-icons">business_center</i>
                     <span>Manager</span>
                 </div>
                 <ul class="list-group list-group-flush">
@@ -17,18 +16,16 @@ module.exports.generators = {
                     <li class="list-group-item">Office Number: ${officeNumber}</li>
                 </ul>
               </div>
-          </div>
-        </section>
       `;
-    },
-    // Create the project section
-    engineer: ({ name, id, email, github }) => {
-        return `
-        <section class="my-3" id="engineer">
-          <div class="flex-row justify-space-between">
-          <div class="card border border-info" style="width: 18rem;">
-                <div class="card-header">
-                    ${name}
+  },
+  // Create the project section
+  engineer: ({ name, id, email, github }) => {
+    return `
+          <div class="card col-3 border border-info" style="width: 18rem;">
+                <div class="card-header p-3 mb-2 bg-primary text-white text-center">
+                    <h2>${name}</h2>
+                    </br>
+                    <i class="icon large material-icons">laptop_mac</i>
                     <span>Engineer</span>
                 </div>
                 <ul class="list-group list-group-flush">
@@ -37,19 +34,19 @@ module.exports.generators = {
                     <li class="list-group-item">GitHub: ${github}</li>
                 </ul>
               </div>
-          </div>
-        </section>
       `;
-    },
+  },
 
-    // create the about section
-    intern: ({name, id, email, school}) => {
-        return `
+  // create the about section
+  intern: ({ name, id, email, school }) => {
+    return `
         <section class="my-3" id="intern">
           <div class="flex-row justify-space-between">
-          <div class="card border border-info" style="width: 18rem;">
-                <div class="card-header">
-                    ${name}
+          <div class="card  col-3 border border-info" style="width: 18rem;">
+                <div class="card-header p-3 mb-2 bg-primary text-white text-center">
+                    <h2>${name}</h2>
+                    </br>
+                    <i class="icon large material-icons">school</i>
                     <span>Intern</span>
                 </div>
                 <ul class="list-group list-group-flush">
@@ -61,12 +58,12 @@ module.exports.generators = {
           </div>
         </section>
       `;
-    }
+  }
 }
 // destructure page data by section
 
 module.exports.generatePage = html => {
-    return `
+  return `
     <!DOCTYPE html>
     <html lang="en">
   
@@ -88,7 +85,9 @@ module.exports.generatePage = html => {
         </div>
       </header>
       <main class="container my-5">
-        ${html}
+        <div class="row">
+            ${html}
+        </div>  
       </main>
       <footer class="container text-center py-3">
         <h3 class="text-dark">&copy; 2020 by Nicolas Esquivel</h3>
